1397f5355c8c3c4660625d3eeabdf7545b0fa85f
jharvard <gsejourne@college.harvard.edu>
2014-12-03 21:08:00 -0500

    index

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
     // configuration
     require_once("../includes/config.php");
-
+    
 ?>
 
 <!DOCTYPE html>
@@ -10,24 +10,27 @@
 <html lang="en-us">
   <head>
     <meta charset="utf-8">
-    <title>Example</title>
+    <meta content="width=device-width" name="viewport">
+    <title>Office Hour Buddy</title>
   </head>
   <body>
-    <?php 
-    
+    <?php
+
         if (isset($_SESSION["user"]))
         {
             echo "<div>You are logged in.  <a href='logout.php'>Log out</a>.</div>";
             echo "<div>Your unique ID for this site is <b>" . htmlspecialchars($_SESSION["user"]["identity"]) . "</b>.</div>";
             if (isset($_SESSION["user"]["fullname"]))
-                echo "<div>Your name is <b>" . htmlspecialchars($_SESSION["user"]["fullname"]) . "</b>.</div>";
-            if (isset($_SESSION["user"]["email"]))
+                echo "<div>Your name is <b>" . htmlspecialchars($_SESSION["user"]["fullname"]) . "</b>.</div>"; ?>
+                <form action="name.php" method="post"><div class="form-group"><button type="submit" class="btn btn-default">Validate</button></div></form>
+            <?php if (isset($_SESSION["user"]["email"]))
                 echo "<div>Your email address is <b>" . htmlspecialchars($_SESSION["user"]["email"]) . "</b>.</div>";
+
         }
         else
             echo "You are not logged in.  <a href='login.php'>Log in</a>.";
 
-      ?>
-    </ul>
+    ?>
+     </ul>
   </body>
 </html>
